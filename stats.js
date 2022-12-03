c9549a9e16bfbc67fd20f028b76e934a0b962b87
Yoda <yoda@yodacode.xyz>
2022-12-02 22:56:46 -0800

    Oops

--- stats.js
+++ stats.js
@@ -2,4 +2,4 @@ import fs from 'fs';
 
 const md = fs.readFileSync('./template.md', 'utf8');
 
-fs.writeFileSync('README.md', md.replace('{{%stats%}}', fs.readdirSync('.').filter(a => +a).map(a => +a).sort((a, b) => (a - b)).map(day => fs.readFileSync(`./${day}/index.js`, 'utf8').split('\n').length - 1).filter(a => a).map((a, i) => `Day ${i + 1} | ${a + 1} | [\`./${a + 1}/index.js\`](https://github.com/YodaLightsabr/aoc22/blob/main/${a + 1}/index.js)`).join('\n')), 'utf8');
+fs.writeFileSync('README.md', md.replace('{{%stats%}}', fs.readdirSync('.').filter(a => +a).map(a => +a).sort((a, b) => (a - b)).map(day => fs.readFileSync(`./${day}/index.js`, 'utf8').split('\n').length - 1).filter(a => a).map((a, i) => `Day ${i + 1} | ${a + 1} | [\`./${i + 1}/index.js\`](https://github.com/YodaLightsabr/aoc22/blob/main/${i + 1}/index.js)`).join('\n')), 'utf8');
